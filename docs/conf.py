f50e277399af4d62f835c72e55b0305e793d089d
Tuan PM <tuanpm@live.com>
2017-05-22 11:33:19 +0700

    remove sphinx google analyzer ext

--- docs/conf.py
+++ docs/conf.py
@@ -39,10 +39,9 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
-    'sphinxcontrib.youtube',
-    'sphinxcontrib.googleanalytics']
+    'sphinxcontrib.youtube']
 
-googleanalytics_id = 'UA-60470603-07'
+# googleanalytics_id = 'UA-60470603-07'
 
 html_theme_options = {
     'collapse_navigation': False,
